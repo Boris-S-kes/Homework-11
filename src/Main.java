157b3e5e2b887ad6dab17f0b21de2c68a7685c0e
msi <https://github.com/Boris-S-kes/Homework-11.git>
2026-07-02 11:29:27 +0400

    Dz -11 (3)

--- src/Main.java
+++ src/Main.java
@@ -25,28 +25,29 @@ public class Main {
     }
 
     public static int calculationDeliveryDays(int deliveryDistance) {
-         int days =0;
         if (deliveryDistance <= 20) {
-            days =1;
             return 1;
         }
         if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-            days +=2;
             return 2;
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            days +=3;
-            System.out.println(" Потребуется дней" + days);
             return 3;
-        }
-        if (deliveryDistance > 100) {
+        } else {
             System.out.println(" Доставки нет");
             return -1;
-        } else {
-            return 0;
+        }
+
+
+
 
 
         }
-    }
+
+
+
+
+
+
 
 
 
@@ -62,6 +63,9 @@ public class Main {
          int deliveryDistance = 95;
          int days = calculationDeliveryDays(deliveryDistance);
          System.out.println("Потребуется дней" + days);
+         int  intervalDostavki = 90;
+
+
 
 
     }
